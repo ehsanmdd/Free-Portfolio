0e78ffd7bdfa015a9b0a9c3dd3dd7f4423ea2c4e
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2022-06-06 06:17:36 +0430

    Main Section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menu = document.querySelector(".menu");
+const showCover = document.querySelector(".cover");
 
 function openNavMenu (){
     navToggleIcon.classList.toggle("nav__toggle-icon--open");
     menu.classList.toggle("menu--open")
+    showCover.classList.toggle("cover--show")
 };
 
 navToggleIcon.addEventListener("click", openNavMenu);
