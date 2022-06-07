7690d17b32619f9490120adf20f3b30ec502f3cd
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2022-06-07 14:42:55 +0430

    Main Section -Resume-

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menu = document.querySelector(".menu");
 const showCover = document.querySelector(".cover");
+const resumeListItems = document.querySelectorAll(".resume-list__item")
 
 function openNavMenu (){
     navToggleIcon.classList.toggle("nav__toggle-icon--open");
@@ -9,4 +10,12 @@ function openNavMenu (){
 };
 
 navToggleIcon.addEventListener("click", openNavMenu);
-menu.addEventListener("click", openMenu)
+menu.addEventListener("click", openNavMenu);
+
+
+resumeListItems.forEach(resumeListItems =>{
+    resumeListItems.addEventListener("click", function () {
+        document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active")
+        resumeListItems.classList.add("resume-list__item--active")
+    })
+})
